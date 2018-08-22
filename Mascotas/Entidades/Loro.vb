89528f2d9b1fab88549486a2167e60463bc109bb
Daniel <Daniel@DESKTOP-ISVANOC>
2018-08-22 18:19:24 -0300

    tp3

--- Mascotas/Entidades/Loro.vb
+++ Mascotas/Entidades/Loro.vb
@@ -2,7 +2,27 @@
     Private _edad As Short
     Private _fechaNacimiento As Date
     Private _nombre As String
-    Private _memoria As Queue
+    Private _memoria As String
+
+    Public Sub New()
+        Me.FechaNacimiento = Date.Now()
+        Me.Nombre = "Anonimo"
+    End Sub
+
+    Public Sub New(FechaNacimiento As Date, Nombre As String)
+        Me.FechaNacimiento = FechaNacimiento
+        Me.Nombre = Nombre
+    End Sub
+
+    Public Property Memoria As Queue
+        Get
+            Return Memoria.Dequeue
+        End Get
+        Set(value As Queue)
+
+        End Set
+    End Property
+
 
     Public ReadOnly Property Edad As Short
         Get
@@ -28,8 +48,17 @@
         End Set
     End Property
 
+    Private Function CalcularEdad(FechaNacimiento As Date) As UShort
+        Return Year(Date.Now) - Year(FechaNacimiento)
+    End Function
 
+    Public Sub Escuchar(Frase As String)
+        _memoria
+    End Sub
 
+    Public Function Hablar() As String
+
+    End Function
 
 
 End Class
